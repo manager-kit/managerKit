bc43061c5838acbfee333ac38e43a764b67f6590
student <rmimamov@edu.hse.ru>
2021-12-11 18:23:44 +0300

    init

---
+++ src/main/java/ru/hse/managerkit/ManagerkitApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.managerkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hse/managerkit/ManagerkitApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.managerkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
