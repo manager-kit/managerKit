73050227662cf0f136b125f1dfb505a203f849a1
rmimamov <rad.imamow@yandex.ru>
2021-12-18 16:38:43 +0300

    repository

--- src/main/java/ru/hse/managerkit/repository/UserRepository.java
+++ src/main/java/ru/hse/managerkit/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package ru.hse.managerkit.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.hse.managerkit.model.QUser;
 import ru.hse.managerkit.model.User;
 
-public interface UserRepository extends BaseRepository<User,Long, QUser>{
+@Repository
+public interface UserRepository extends BaseRepository<User, Long, QUser> {
 }
